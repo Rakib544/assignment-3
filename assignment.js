e23882152939a17063897296a9f6d68f2ec63e98
Rakib544 <md.rakib10122003@gmail.com>
2021-01-21 22:43:08 +0600

    first problem done

--- assignment.js
+++ assignment.js
@@ -1,6 +1,10 @@
 
 function kilometerToMeter(kilometer) {
     var meter = kilometer * 1000; //converting kilometer to meter
+    if(kilometer < 0) { // checking if the value of kilometer is negative or not
+        return "distance can't be negative"
+    }
     return meter // returning meter value
 }
 
+function 
